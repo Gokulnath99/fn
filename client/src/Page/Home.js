d6eaa4a670fe54eabc34121ba98b0296ba305edd
Gokulnath99 <gokulnathanand23@gmail.com>
2020-08-14 23:22:40 +0530

    Initial Commit!

---
+++ client/src/Page/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import AppBar from '../Components/AppBar'
+
+class Homepage extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Home</h1>
+                <AppBar/>
+            </div>
+            
+        );
+    }
+}
+
+export default Homepage;
+  
